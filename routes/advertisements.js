ef61437112e1a21d742b6188acb2d28906d8ffda
Dmitryi Sachko <diman.on5lab@gmail.com>
2019-04-27 07:31:29 +0300

    испоользование шаблона

--- routes/advertisements.js
+++ routes/advertisements.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('advertisements/index', { title: 'Express' });
 });
 
+router.get('/:id', function(req, res, next) {
+  console.log(req.query);
+  res.render('advertisements/show', { title: 'Express' , data: req.params.id});
+});
+
 module.exports = router;
